95a3829bb530328c3d37b441d8fc3ad842dc7dd3
770grappenmaker <770grappenmaker@gmail.com>
2023-10-23 12:31:09 +0200

    Add packages repository and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
+    `maven-publish`
 }
 
 repositories {
@@ -18,4 +19,22 @@ kotlin {
 dependencies {
     api("org.ow2.asm:asm:9.4")
     api("org.ow2.asm:asm-commons:9.4")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("lib") { from(components["java"]) }
+    }
+
+    repositories {
+        maven {
+            name = "Packages"
+            url = uri("https://maven.pkg.github.com/770grappenmaker/mappings-util")
+
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
