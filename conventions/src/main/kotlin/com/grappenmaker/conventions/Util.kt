ee939f41b298e011794d115aa137bccf05a8124a
Koen Oostveen <koen@koenoostveen.nl>
2026-06-05 22:26:49 +0200

    Automatically build docs with forgejo actions

--- conventions/src/main/kotlin/com/grappenmaker/conventions/Util.kt
+++ conventions/src/main/kotlin/com/grappenmaker/conventions/Util.kt
@@ -3,8 +3,11 @@ package com.grappenmaker.conventions
 import org.gradle.api.Project
 import kotlin.text.trim
 
+// TODO: handle nulls and errors, don't care for now.
 @Suppress("UnstableApiUsage")
-fun Project.sopsDecrypt(resource: String): String = providers.exec {
-    commandLine("sops", "decrypt", resource)
-    workingDir(rootProject.projectDir)
-}.standardOutput.asText.get().trim()
+fun Project.sopsDecrypt(resource: String): String? = runCatching {
+    providers.exec {
+        commandLine("sops", "decrypt", resource)
+        workingDir(rootProject.projectDir)
+    }.standardOutput.asText.get().trim()
+}.getOrNull()
